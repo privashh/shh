4af5ef04bb2f1e8a2387a4846438716b208962a4
privashh <268088310+privashh@users.noreply.github.com>
2026-04-02 17:38:40 +0900

    feat(sdk): browser-safe barrel export

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,9 @@
+// Browser-safe entry point: notes, Poseidon, Merkle tree, witness-input builders, proof
+// formatting. No snarkjs / node builtins. For Groth16 proving in Node, import "@shh/sdk/node".
+export * from "./constants";
+export * from "./poseidon";
+export * from "./random";
+export * from "./merkleTree";
+export * from "./proof";
+export * from "./pool";
+export * from "./utxo";
